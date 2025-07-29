b8b12c23b391e78b21fe028239bd7d50214a8ec6
Rajendra Phani Vithal Chaganti <rajendra2006ch@gmail.com>
2025-07-29 15:03:28 +0530

    Exp-7-A-PrintingArrayElements.cpp

---
+++ Exp-7-A-PrintingArrayElements.cpp
@@ -0,0 +1,23 @@
+/*
+Rajendra Chaganti 
+PRN : 24070123150
+B3
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = 5;
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
+/*
+Output :
+10
+20
+30
+40
+50
+*/
