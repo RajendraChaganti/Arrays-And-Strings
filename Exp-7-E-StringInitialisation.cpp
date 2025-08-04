3f706f2eded87d90926b06adf0532cfbf41e4d9c
Rajendra Phani Vithal Chaganti <rajendra2006ch@gmail.com>
2025-08-04 15:10:34 +0530

    Exp-7-E-StringInitialisation.cpp

---
+++ Exp-7-E-StringInitialisation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+int main() {
+    // 1 string (array)
+    char str1[] = "Hello my name is Rajendra Chaganti,I am an E&TC student in SIT Pune.";
+    // 2 std::string initialization
+    std::string str2 = "My mom's name is Nageswari.";
+    // Output of both strings printed one by one below this line of string array and string std initialisation
+    std::cout << str1 << std::endl;
+    std::cout << str2 << std::endl;
+    return 0;
+}
+/*
+Output:
+Hello my name is Rajendra Chaganti,I am an E&TC student in SIT Pune.
+My mom's name is Nageswari.
+*/
