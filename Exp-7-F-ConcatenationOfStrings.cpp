5c6b42bba8c5c69f6010278ad0c5d3c350da3653
Rajendra Phani Vithal Chaganti <rajendra2006ch@gmail.com>
2025-08-04 15:19:11 +0530

    Exp-7-F-ConcatenationOfStrings.cpp

---
+++ Exp-7-F-ConcatenationOfStrings.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+int main() {
+    std::string str1 = "Hello My name is ";
+    std::string str2 = "Rajendra Chaganti.";
+    // Concatenate using + operator
+    std::string result = str1 + str2;
+    // Output the result
+    std::cout << result << std::endl;
+    return 0;
+}
+/*
+Output :
+Hello My name is Rajendra Chaganti.
+*/
+
